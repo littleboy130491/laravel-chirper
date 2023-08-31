99eb1aec96829950e94fefd299cfea64df05b649
littleboy_13 <littleboy_13@yahoo.com>
2023-08-31 07:35:50 +0700

    My First Commit

---
+++ resources/views/chirps.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    This will display all chirps.
+</x-app-layout>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        This is header
+    </x-slot>
+    <div class="h-100 white py-4 px-4">
+    This is slot
+    </div>
+    
+</x-app-layout>
